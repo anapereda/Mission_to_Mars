c165f663aad0ca0840b94834092d5ee5ebf8072b
anapereda <65132236+anapereda@users.noreply.github.com>
2020-07-12 11:55:47 -0700

    scrapping

---
+++ scraping.py
@@ -0,0 +1,95 @@
+#import dependencies
+
+
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+
+# Set the executable path and initialize the chrome browser in splinter
+executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+browser = Browser('chrome', **executable_path)
+
+
+def mars_news(browser):
+    
+    # Visit the mars nasa news site
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+
+    # Optional delay for loading the page One is that we’re searching for elements 
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find("div", class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+    except AttributeError:
+        return None, None
+    
+    return news_title, news_p
+
+
+# ## JPL Space Images Featured Image
+
+def featured_image(browser):
+    
+    # Visit URL
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
+    
+    # Find the more info button and click that has the provided text
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.links.find_by_partial_text('more info')
+    more_info_elem.click()
+
+
+    # Parse the resulting html with soup; conver to html to obtain full size image
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+
+    except AttributeError:
+        return None
+
+    # Use the base URL to create an absolute URL
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+    
+    return img_url
+
+##Mars Facts
+
+def mars_fact():
+
+    try:
+        
+        #read_html() searches and returns a list of tables
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+
+    except BaseException:
+        return None
+    
+
+    df.columns=['description', 'value']
+    df.set_index('description', inplace=True)
+
+    return df.to_html()
+
+browser.quit()
+
+
+
+
